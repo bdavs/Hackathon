36300d2b138530e042adbb9c9de224424f7f2007
Robert Davis <rdavis477@gmail.com>
2017-12-20 17:42:53 -0500

    function-ify interface.py and add boilerplate for admin screen

--- interface.py
+++ interface.py
@@ -2,31 +2,50 @@ from tkinter import *
 from tkinter import ttk
 import readingfile as r
 
-root = Tk()
+def display():
+    root = Tk()
 
-height = 3 #make number dynamic for number of entries
-width = 3 #update number
+    height = 3 #make number dynamic for number of entries
+    width = 3 #update number
 
-d = r.fileToDict() #import data from file
+    d = r.fileToDict() #import data from file
 
-#error checking
-#may require own function
-if d is -1:
-    error=Label(root, text="No entries in table")
-    error.grid(row=0,column=0)
-    mainloop()
-    quit()
+    #error checking
+    #may require own function
+    if d is -1:
+        error=Label(root, text="No entries in table")
+        error.grid(row=0,column=0)
+        mainloop()
+        quit()
 
-for i in range(len(d['name'])): #Rows
-    #for v in val: #range(width): #Columns
-    e1=Label(root, text=d['name'][i])
-    e2=Label(root, text=d['date'][i])
-    e1.grid(row=i+1, column=0)
-    e2.grid(row=i+1, column=1)
-    #add remaining fields
-header=Label(root, text="Event Name")
-header.grid(row=0,column=0)
-header=Label(root, text="Event Date")
-header.grid(row=0,column=1)
+    for i in range(len(d['name'])): #Rows
+        #for v in val: #range(width): #Columns
+        e1=Label(root, text=d['name'][i])
+        e2=Label(root, text=d['date'][i])
+        e1.grid(row=i+1, column=0)
+        e2.grid(row=i+1, column=1)
+        #add remaining fields
+    header=Label(root, text="Event Name")
+    header.grid(row=0,column=0)
+    header=Label(root, text="Event Date")
+    header.grid(row=0,column=1)
 
+def adminScreen():
+    root = Tk()
+
+    for i in range(3): #Rows
+        #for v in val: #range(width): #Columns
+        e1=Entry(root, text='')
+        e2=Entry(root, text='')
+        e1.grid(row=i+1, column=0)
+        e2.grid(row=i+1, column=1)
+
+    header=Label(root, text="Event Name")
+    header.grid(row=0,column=0)
+    header=Label(root, text="Event Date")
+    header.grid(row=0,column=1)
+
+
+adminScreen()
+#display()
 mainloop()
