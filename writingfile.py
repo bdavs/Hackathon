ba2b42cb507eae2e4a547baf5c3979b009c81a37
Ssaurabh Kesari <Ssaurabhkesari@Ssaurabhs-MacBook-Pro.local>
2017-12-21 15:00:52 -0500

    updated the indents of the code post the def function

--- writingfile.py
+++ writingfile.py
@@ -3,18 +3,15 @@ import os.path
 
 def writeToFile(Dict):
 
-#needsHeader = True
-#if os.path.exists('testfile.csv'):
-#    needsHeader = False
 
 #connect with interface
-with open('testfile.csv', 'a') as csvfile:
-    fieldnames = ['name', 'date', 'speaker']
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-    for i in range(len(Dict['name']))
-        name=Dict['name'][i]
-        date=Dict['date'][i]
-        speaker=Dict['speaker'][i]
-        writer.writerow({'name': name, 'date': date, 'speaker':speaker})
-	#add remaining field
-csvfile.close()
+    with open('testfile.csv', 'a') as csvfile:
+        fieldnames = ['name', 'date', 'speaker']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        for i in range(len(Dict['name']))
+            name=Dict['name'][i]
+            date=Dict['date'][i]
+            speaker=Dict['speaker'][i]
+            writer.writerow({'name': name, 'date': date, 'speaker':speaker})
+ 	 #add remaining field
+    csvfile.close()
